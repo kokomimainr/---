39e8af0d594ae672e4f1a084cdf45e5c7627734e
Leonid Shifrin <leonids@wolfram.com>
2024-07-19 15:46:37 +0300

    Added weapons

---
+++ core-async-boomerang-cats-warriors/src/game-models/Shuriken .js
@@ -0,0 +1,36 @@
+class Shuriken {
+    constructor(trackLength) {
+      this.skin = '⚡';
+      this.position = -1;
+      this.trackLength = trackLength;
+    }
+  
+    fly() {
+      const distance = 10;
+  
+      for (let i = 0; i <= distance; i += 1) {
+        setTimeout(() => this.moveRight(1), 30 * i);
+      }
+  
+      for (let i = 0; i <= distance; i += 1) {
+        setTimeout(() => this.moveLeft(1), 30 * (distance + 1));
+      }
+  
+      setTimeout(() => this.reset(), 30 * (distance * 2));
+    }
+  
+    reset() {
+      this.position = -1;
+    }
+  
+    moveLeft(distance) {
+      this.position -= distance;
+    }
+  
+    moveRight(distance) {
+      this.position += distance;
+    }
+  }
+  
+  module.exports = Shuriken;
+  
